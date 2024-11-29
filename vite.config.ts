6baf1dfcf76ad45c8b5f90a854a0b3bf807e9a1c
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-11-29 19:48:37 -0300

    build: adjust application port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port: 3001
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
